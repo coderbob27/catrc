1bee42257c5883e50851cc45ea1653535d826c0d
coderbob27 <zloymudvin@yandex.ru>
2024-06-03 00:02:38 +0300

    phpstorm

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
             $document = new Document('https://pik.ru/search', true);
             $sub_headings = $document->find('.styles__PriceRowValue-sc-15v0uww-4::text()');
 
+
             foreach($sub_headings as $sub) {
                 echo $sub;
             }
