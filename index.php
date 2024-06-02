60c6147f114badc95c7f20ba5469618b5b6c100a
coderbob27 <zloymudvin@yandex.ru>
2024-06-02 23:59:52 +0300

    phpstorm

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
             use DiDom\Document;
             $document = new Document('https://pik.ru/search', true);
             $sub_headings = $document->find('.styles__PriceRowValue-sc-15v0uww-4::text()');
+
             foreach($sub_headings as $sub) {
                 echo $sub;
             }
